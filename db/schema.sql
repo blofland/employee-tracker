59a6a3cc39b4debee030fba04419f81049aeb68f
unknown <boedi15@gmail.com>
2021-12-01 19:52:26 -0700

    added tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,27 @@
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
 
-CREATE TABLE parties (
+CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   description TEXT
 
 
-CREATE TABLE candidates (
+CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+
+  CREATE TABLE role (
+      id INTEGER AUTO_INCREMENT PRIMARY KEY,
+      title VARCHAR(30) NOT NULL,
+      salary DECIMAL,
+      department_id INTEGER
+
+
+  )
 );
